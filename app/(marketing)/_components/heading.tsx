597566843c84d2fae7294cc240d460960e1b13c1
Rian <riangabrieldev@gmail.com>
2024-10-07 16:18:22 -0300

    Criação do marketing e _componentsa

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export const Heading = () => {
+  return (
+    <div
+      className="
+    max-w-3xl
+    space-y-4"
+    >
+      <h1>
+        Suas Ideias, Documentos, e Planos. Unificados, Bem Vindo ao{" "}
+        <span className="underline">Horseon</span>
+      </h1>
+    </div>
+  );
+};
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,27 @@
+import { Heading } from "./_components/heading";
+
+const MarketingPage = () => {
+  return (
+    <div
+      className="
+      min-h-full
+      flex
+      flex-col"
+    >
+      <div
+        className="
+        flex
+        flex-col
+        items-center
+        justify-center
+        md:justify-start
+        text-center
+        gap-y-8"
+      >
+        <Heading />
+      </div>
+    </div>
+  );
+};
+
+export default MarketingPage;
