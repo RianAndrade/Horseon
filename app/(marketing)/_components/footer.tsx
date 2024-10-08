7cbdbbbca43fd12d721aff7fd531e510cf48ec26
Rian <riangabrieldev@gmail.com>
2024-10-08 14:20:05 -0300

    Criação do do footer  para o marketing

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button";
+import { Logo } from "./logo";
+
+export const Footer = () => {
+  return (
+    <div
+      className="
+      flex
+      items-center
+      w-full
+      p-6
+      bg-background
+      z-50"
+    >
+      <Logo />
+      <div
+        className="
+        flex
+        md:ml-auto
+        w-full
+        justify-between
+        md:justify-end
+        items-center
+        gap-x-2
+        text-muted-foreground"
+      >
+        <Button variant="ghost" size="sm">
+          Privacy Policy
+        </Button>
+        <Button variant="ghost" size="sm">
+          Terms &amp; Conditions
+        </Button>
+      </div>
+    </div>
+  );
+};
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./_components/footer";
 import { Heading } from "./_components/heading";
 import { Heroes } from "./_components/heroes";
 
@@ -25,6 +26,7 @@ const MarketingPage = () => {
       >
         <Heading />
         <Heroes />
+        <Footer />
       </div>
     </div>
   );
