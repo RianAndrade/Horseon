2bf3278c79fa9a386a24258874a06b569ed8790f
Rian <riangabrieldev@gmail.com>
2024-10-08 15:07:22 -0300

    Criação do hook use-scroll e aplicação do mesmo na navbar

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { cn } from "@/lib/utils";
+
 export const Navbar = () => {
+  const scrolled = useScrollTop();
+
   return (
-    <div>
-      <p>rolassss </p>
+    <div className={cn( "z-50 bg-background top-0 flex items-cente w-full p-6", scrolled && "borde-b shadow-sm"",
+    )}>
+      <p>rolassss</p>
     </div>
+
   );
 };
